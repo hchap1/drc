3c2fdd53a820aa5e387d412f3784360397e86fe8
Harrison Chapman <hchap1@hotmail.com>
2026-06-20 00:11:08 +1000

    feat: new boot with networking

--- boot.py
+++ boot.py
@@ -1,5 +1,13 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import webrepl
-webrepl.start()
+import network, webrepl
+
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid='drc-esp', password='drc123')
+
+while not ap.active():
+    pass
+
+webrepl.start(password='drc123')
